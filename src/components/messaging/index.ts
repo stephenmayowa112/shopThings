97041c7674ae8b538c5b94626a17e413c0c42062
stephenmayowa112 <stephenmayowa112@gmail.com>
2026-01-03 13:28:25 +0100

    Fix build errors: messaging types, date-fns dependency, and type casting

--- src/components/messaging/index.ts
+++ src/components/messaging/index.ts
@@ -2,3 +2,4 @@ export { ConversationList } from './ConversationList';
 export { MessageThread } from './MessageThread';
 export { MessagingContainer } from './MessagingContainer';
 export { StartConversationButton } from './StartConversationButton';
+export { StartConversationButton } from './StartConversationButton';
